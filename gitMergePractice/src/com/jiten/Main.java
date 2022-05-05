0bb9e5d4f0524920bd82ce0c556bc695499cdab3
jitendra <netij.poudel@gmail.com>
2022-05-04 22:50:02 -0500

    added new project

---
+++ gitMergePractice/src/com/jiten/Main.java
@@ -0,0 +1,9 @@
+package com.jiten;
+
+public class Main {
+    public static void main(String[] args) {
+        Service service = new Service();
+        service.setServiceId("1");
+        service.setServiceName("abcd");
+    }
+}
---
+++ gitMergePractice/src/com/jiten/Service.java
@@ -0,0 +1,22 @@
+package com.jiten;
+
+public class Service {
+    String serviceName;
+    String serviceId;
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public String getServiceId() {
+        return serviceId;
+    }
+
+    public void setServiceId(String serviceId) {
+        this.serviceId = serviceId;
+    }
+}
